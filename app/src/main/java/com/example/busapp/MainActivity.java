b6c2d590cf20aa962dda5d54830fd949c1717381
NSCakir <equotecakir@gmail.com>
2019-11-09 16:06:20 -0500

    commit

--- app/src/main/java/com/example/busapp/MainActivity.java
+++ app/src/main/java/com/example/busapp/MainActivity.java
@@ -14,11 +14,17 @@ public class MainActivity extends AppCompatActivity {
         Thread updateThread = new Thread(new UpdateThread());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mTitleWindow = (TextView) findViewById(R.id.messageWindow);
-        TextView mMessageWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.messageWindow);
 
 
         mTitleWindow.setText("Stops");
         StringBuilder stringBuilder = new StringBuilder();
+
+        String someMessage = " This is a message ";
+        for(int i = 0; i < 100; i++){
+            stringBuilder.append(someMessage);
+        }
+        mMessageWindow.setText(stringBuilder.toString());
     }
 }

b73e24d21300c9f217379975751087faac5d51b1
Michael Dressner <michaeldressner@gmail.com>
2019-11-09 15:38:21 -0500

    Added thread to main activity

--- app/src/main/java/com/example/busapp/MainActivity.java
+++ app/src/main/java/com/example/busapp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Thread updateThread = new Thread(new UpdateThread());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

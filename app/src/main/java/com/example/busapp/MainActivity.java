789930010c38c407cbe3264dab87ed859ac0be36
NSCakir <equotecakir@gmail.com>
2019-11-09 15:38:57 -0500

    Added a button and bar similar to the one in the seminar.

--- app/src/main/java/com/example/busapp/MainActivity.java
+++ app/src/main/java/com/example/busapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.busapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mTitleWindow = (TextView) findViewById(R.id.messageWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.titleWindow);
     }
 }
